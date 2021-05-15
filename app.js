7c3f2d7930b36e4d0d0966e7af54f21a88227593
Andrew-Irving <amirving94@gmail.com>
2021-05-15 09:04:36 -0400

    added error function and visit page anchor

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
                 /// Selectors ///
 // root url
 const APIurl = 'https://api.github.com/users/'
+// HTML IDs
 const form = document.getElementById('form')
 const search = document.getElementById('user-search')
 const button = document.getElementById('user-submit')
+const main = document.getElementById('main')
 
                 /// Evnets ///
+// searches username and profile
 button.addEventListener('click', (e) => {
     e.preventDefault()
 
@@ -21,8 +24,47 @@ button.addEventListener('click', (e) => {
 async function getUser(username) {
     try {
         const res = await axios(APIurl + username)
-        console.log(res.data)
+        createStarcard(res.data)
     } catch(error) {
-        console.log(error)
+        if(error.response.status == 404) {
+            createErrorCard('No profile with that username')
+        }
     }
+}
+
+function createStarcard (user) {
+    const newHTML = `
+<div class="starcard">
+    <div>
+        <img src="${user.avatar_url}" alt="user image" class="starcard_avatar">
+    </div>
+    <div class="starcard_user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+
+        <ul>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+        </ul>
+
+        <div class="starcard_repos">
+            <a href="" class="repos">Repo 1</a>
+            <a href="" class="repos">Repo 2</a>
+            <a href="" class="repos">Repo 3</a>
+        </div>
+    </div>
+</div>`
+
+    main.innerHTML = newHTML
+}
+
+// creates an error message when user isn't found
+function createErrorCard (msg) {
+    const errorHTML = `
+        <div class="err-msg">
+            <h1>${msg}</h1>
+        </div>`
+
+    main.innerHTML = errorHTML
 }
